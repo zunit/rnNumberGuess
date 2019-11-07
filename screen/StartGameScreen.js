c1d86227fda9787f5c0502d4ccf44c6ad18aa077
zunit <jackjiananzhou@gmail.com>
2019-11-07 17:26:27 -0500

    add Separate screen and component

---
+++ screen/StartGameScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>Start a New Game!</Text>
+            <View>
+                <Text>Select a number</Text>
+                <TextInput/>
+                <View>
+                    <Button title="Reset" onPress={() => {}}/>
+                    <Button title="Confirm" onPress={() => {}}/>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center', 
+    }, 
+
+});
+
+export default StartGameScreen;
