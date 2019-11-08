bc122b0ef904253879593a4d459d418c476a3b74
zunit <jackjiananzhou@gmail.com>
2019-11-07 21:45:24 -0500

    Input box can be updated to display card output

---
+++ constants/color.js
@@ -0,0 +1,4 @@
+export default {
+    primary: 'white',
+    accent: '#c717fc'
+}
--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -1,22 +1,80 @@
-import React from 'react';
-import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import { 
+    View, 
+    StyleSheet, 
+    Text, 
+    Button, 
+    TouchableWithoutFeedback, 
+    Keyboard 
+} from 'react-native';
 
 // custom import Component
 import Card from './../components/Card';
+import Input from './../components/Input';
+
+// custom import Constant 
+import Color from './../constants/color';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState()
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g,''));
+    };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+            return;
+        }
+        setConfirmed(true);
+        setEnteredValue('');
+        // convert text to a number
+        setSelectedNumber(parseInt(enteredValue));
+    }
+
+    let content;
+
+    if (confirmed){
+        content = <Card><View><Text>{selectedNumber}</Text></View></Card>
+    }
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text style={styles.title}>Select a number</Text>
-                <TextInput style={styles.input}/>
-                <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}}/>
-                    <Button title="Confirm" onPress={() => {}}/>
-                </View>
-            </Card>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text style={styles.title}>Select a number</Text>
+                    <Input
+                        style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType="number-pad"
+                        maxLength={2}
+                        onChangeText = {numberInputHandler}
+                        value = {enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button color={Color.primary} title="Reset" onPress={resetInputHandler} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button color={Color.primary} title="Confirm" onPress={confirmInputHandler} />
+                        </View>
+                    </View>
+                </Card>
+                {content}
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -24,28 +82,34 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignItems: 'center', 
-    }, 
-    title:{
-        fontSize: 20, 
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
         marginVertical: 10,
-        
+
     },
     buttonContainer: {
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
-    }, 
+    },
+
+    button: {
+        width: 100,
+        backgroundColor: '#f7287b',
+        borderRadius: 5
+    },
+
     inputContainer: {
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
-        
+
     },
     input: {
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
+        width: 50,
         alignItems: 'center',
         textAlign: 'center'
     }
